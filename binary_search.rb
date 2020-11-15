f500b5a417bc4a49396620b62a7383f12d340cdd
Kevin Lenehan <klenehan123@gmail.com>
2020-11-15 16:45:22 +0000

    some formatting + driver script added and working

--- binary_search.rb
+++ binary_search.rb
@@ -1,8 +1,7 @@
-require 'pry'
+require "pry"
 
 # binary search node
 class Node
-
   attr_accessor :value, :left, :right
 
   def initialize(value, left, right)
@@ -14,7 +13,6 @@ class Node
   def children
     @left || @right ? true : false
   end
-
 end
 
 # binary search tree
@@ -60,9 +58,8 @@ class Tree
       node.right = node.right.right
     else
       parent = node.right
-      while parent.left.left
-        parent = parent.left
-      end
+      parent = parent.left while parent.left.left
+
       value = parent.left.value
       parent.left = nil
     end
@@ -144,8 +141,7 @@ class Tree
   end
 
   # level order transverse
-  def level_order()
-    print "Level ordering\n"
+  def level_order
     queue_nodes = []
     queue_nodes.push(@root)
     next_level = []
@@ -172,7 +168,7 @@ class Tree
   end
 
   # in order transverse, return array of values
-  def in_order(node=@root, array=[])
+  def in_order(node = @root, array = [])
     return [node.value] unless node.children
 
     if node.left
@@ -189,7 +185,7 @@ class Tree
   end
 
   # pre order transverse, return array of values
-  def pre_order(node=@root, array=[])
+  def pre_order(node = @root, array = [])
     return [node.value] unless node.children
 
     array.push(node.value)
@@ -206,7 +202,7 @@ class Tree
   end
 
   # post order transverse, return array of values
-  def post_order(node=@root, array=[])
+  def post_order(node = @root, array = [])
     return [node.value] unless node.children
 
     if node.left
@@ -222,15 +218,17 @@ class Tree
     array
   end
 
-  def height(node, cnt=0)
+  def height(node, cnt = 0)
     return cnt unless node.children
+
     cnt += 1
     height(node.left, cnt) if node.left
     height(node.right, cnt) if node.right
   end
 
-  def depth(node, dnode=@root, cnt=0)
+  def depth(node, dnode = @root, cnt = 0)
     return cnt if node.value == dnode.value
+
     cnt += 1
     if node.value < dnode.value
       depth(node, dnode.left, cnt)
@@ -239,38 +237,37 @@ class Tree
     end
   end
 
-  def balanced?(node=@root, balance=true)
-
+  def balanced?(node = @root, balance = true)
     return balance unless node.children
 
-    left_height = node.left ? self.height(node.left) : nil
-    right_height = node.right ? self.height(node.right) : nil
+    left_height = node.left ? height(node.left) : nil
+    right_height = node.right ? height(node.right) : nil
     balance = heights_balanced(left_height, right_height)
 
-    balance = self.balanced?(node.left, balance) if node.left && node.left.children && balance
-    balance = self.balanced?(node.right, balance) if node.right && node.right.children && balance
- 
+    balance = balanced?(node.left, balance) if node.left&.children && balance
+    balance = balanced?(node.right, balance) if node.right&.children && balance
+
     balance
   end
-  
+
   def heights_balanced(left, right)
     if left && right && ((left - right).abs <= 1)
-      return true
+      true
     elsif (left.nil? && right == 0) || (left == 0 && right.nil?)
-      return true
+      true
     elsif left.nil? && right.nil?
-      return true
-    else 
-      return false
+      true
+    else
+      false
     end
   end
 
   def rebalance
-    unless self.balanced?
-      array = self.level_order
-      self.build_tree(array, 0, array.length - 1)
-      "Rebalanced"
-    end
+    return if balanced?
+
+    array = level_order
+    build_tree(array, 0, array.length - 1)
+    "Rebalanced"
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -278,46 +275,45 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
-
 end
 
-array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-#array = [1,3,4,6,7,8,10,13,14]
+# driver script
+
+# 1. Create a binary search tree from an array of random numbers (`Array.new(15) { rand(1..100) }`)
+array = Array.new(15) { rand(1..100) }
 sorted_array = array.sort.uniq
 
 bst = Tree.new
 bst.build_tree(sorted_array, 0, sorted_array.length - 1)
 bst.pretty_print
 
-p bst.delete(9)
-p bst.delete(23)
-bst.pretty_print
-p bst.balanced?
-p bst.rebalance
-bst.pretty_print
-p bst.balanced?
+# 2. Confirm that the tree is balanced by calling `#balanced?`
+p bst.balanced? ? "Tree is balanced" : "Tree not balanced"
 
-# p bst.height(bst.find(67))
-# p bst.depth(bst.find(5))
-# bst.insert(5)
-# bst.pretty_print
-# bst.insert(11)
-# bst.pretty_print
-# bst.insert(18)
-# bst.pretty_print
-# bst.delete(7)
-# bst.pretty_print
-# bst.delete(5)
-# bst.pretty_print
+# 3. Print out all elements in level, pre, post, and in order
+p bst.level_order
+p bst.pre_order
+p bst.post_order
+p bst.in_order
 
-# bst.delete(13)
-# bst.pretty_print
-# bst.delete(1)
-# bst.pretty_print
-# bst.delete(14)
-# bst.pretty_print
-# bst.delete(18)
-# bst.pretty_print
-# p bst.in_order
-# p bst.pre_order
-# p bst.post_order
+# 4. try to unbalance the tree by adding several numbers > 100
+bst.insert(105)
+bst.insert(114)
+bst.insert(130)
+
+# 5. Confirm that the tree is unbalanced by calling `#balanced?`
+bst.pretty_print
+p bst.balanced? ? "Tree is balanced" : "Tree not balanced"
+
+# 6. Balance the tree by calling `#rebalance`
+bst.rebalance
+bst.pretty_print
+
+# 7. Confirm that the tree is balanced by calling `#balanced?`
+p bst.balanced? ? "Tree is balanced" : "Tree not balanced"
+
+# 8. Print out all elements in level, pre, post, and in order
+p bst.level_order
+p bst.pre_order
+p bst.post_order
+p bst.in_order
